86812b7a17facc88c6be9783854ff43d0bd7afa0
dandevri <hi@dandevri.es>
2019-06-01 18:50:19 +0200

    Add client js

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ const pluginRss = require("@11ty/eleventy-plugin-rss");
 
 module.exports = function(config) {
   config.addPassthroughCopy("static/img");
+  config.addPassthroughCopy("static/js");
   config.addPassthroughCopy("css/style.css");
   config.addPlugin(pluginRss);
 
