94d7e4b72197e08fbac77abc0cd85a23e946ec10
dandevri <hi@dandevri.es>
2019-07-10 14:19:44 +0200

    Add terser plugin to config

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,12 @@
+import { terser } from "rollup-plugin-terser";
+
 module.exports = {
   input: 'static/js/script.js',
   output: {
     file: '_site/static/js/script.js',
     format: 'iife'
-  }
+  },
+  plugins: [
+    terser(),
+  ]
 };
